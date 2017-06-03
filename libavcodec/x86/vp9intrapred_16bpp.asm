547e93c8dc516cec369e6ea41bdda9b9cb704d95
Ilia <zakne0ne@gmail.com>
2017-06-03 19:00:52 +0700

    fix#2

--- libavcodec/x86/vp9intrapred_16bpp.asm
+++ libavcodec/x86/vp9intrapred_16bpp.asm
@@ -900,11 +900,11 @@ cglobal vp9_ipred_dl_32x32_16, 2, 6, 7, dst, stride, l, a
     vperm2i128              m2, m1, m4, q0201       ; Z......555555555
     
     mova   [dstq+strideq*0+0 ], m0
-    mova   [dstq+strideq*0+16], m1
+    mova   [dstq+strideq*0+32], m1
     vpalignr                m3, m1, m0, 2
     vpalignr                m4, m2, m1, 2
     mova   [dstq+strideq*1+0 ], m3
-    mova   [dstq+strideq*1+16], m4
+    mova   [dstq+strideq*1+32], m4
     RET
 %endif
 
