bdeec103a793bf3cd331d3666a996e4857aeddbb
Ilia Valiakhmetov <zakne0ne@gmail.com>
2017-07-03 03:05:35 +0700

    fix#7

--- libavcodec/x86/vp9intrapred_16bpp.asm
+++ libavcodec/x86/vp9intrapred_16bpp.asm
@@ -1545,8 +1545,8 @@ cglobal vp9_ipred_vl_16x16_16, 2, 4, 6, dst, stride, l, a
     mova                    m0, [aq]                ; abcdefghijklmnop
     vpbroadcastw           xm5, [aq+30]             ; pppppppp
     vperm2i128              m1, m0, m5, q0201       ; ijklmnoppppppppp
-    vpalignr                m2, m5, m0, 2           ; bcdefghijklmnopp
-    vpalignr                m3, m5, m0, 4           ; cdefghijklmnoppp
+    vpalignr                m2, m1, m0, 2           ; bcdefghijklmnopp
+    vpalignr                m3, m1, m0, 4           ; cdefghijklmnoppp
     mova                    m4, m2
     pavgw                   m4, m0
     LOWPASS                  0, 2, 3                ; BCDEFGHIJKLMNOPp
