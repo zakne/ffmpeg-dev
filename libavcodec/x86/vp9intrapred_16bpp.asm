484ef72257d790c2fc00b8d299662831fa61061b
Ilia Valiakhmetov <zakne0ne@gmail.com>
2017-07-03 16:02:26 +0700

    test#3

--- libavcodec/x86/vp9intrapred_16bpp.asm
+++ libavcodec/x86/vp9intrapred_16bpp.asm
@@ -957,10 +957,12 @@ cglobal vp9_ipred_dl_32x32_16, 2, 6, 7, dst, stride, l, a
     mova    [dstq+stride3q+ 32], m4
     mova   [dst16q+stride3q+ 0], m4
     mova   [dst16q+stride3q+32], m6
-    vperm2i128               m5, m5, m2, q0201
+    ;vpalignr                 m3, m5, m0, 16
+    ;vpalignr                 m4, m2, m1, 16
+    vperm2i128               m5, m5, m1, q0201
+    vperm2i128               m2, m2, m2, q0101
     mova                     m0, m5
     mova                     m1, m2
-    mova                     m2, m6
     lea                    dstq, [dstq+strideq*4]
     lea                  dst16q, [dst16q+strideq*4]
     dec                    cntd
