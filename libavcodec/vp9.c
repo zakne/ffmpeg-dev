c29dd4384b5ba984751fbe3ae8d1af0809f51c6d
Ilia Valiakhmetov <zakne0ne@gmail.com>
2017-08-04 04:00:13 +0700

    threadnr

--- libavcodec/vp9.c
+++ libavcodec/vp9.c
@@ -1201,12 +1201,12 @@ int decode_tiles(AVCodecContext *avctx, void *tdata, int jobnr, int threadnr)
         }
         //fix from here
         int row_i;
-        row_i = (jobnr - (jobnr % s->s.h.tiling.tile_cols)) / s->s.h.tiling.tile_cols;
+        row_i = (threadnr - (threadnr % s->s.h.tiling.tile_cols)) / s->s.h.tiling.tile_cols;
         
         pthread_mutex_lock(&s->mutex);
         s->m_row[row_i]++;
 
-        if ((jobnr%s->s.h.tiling.tile_cols) != 0) {
+        if ((threadnr%s->s.h.tiling.tile_cols) == 0) {
             s->cur_lflvl_ptr = td->lflvl_ptr;
             s->cur_row = td->tile_row_start;
             s->cur_uvoff = td->uvoff;
