731fac3f3d64c9a890390d5c8ad15786ecc53c41
Ilia Valiakhmetov <zakne0ne@gmail.com>
2017-08-09 23:26:01 +0700

    fix#5

--- libavcodec/vp9.c
+++ libavcodec/vp9.c
@@ -1106,7 +1106,7 @@ static void free_buffers(VP9Context *s)
 
     av_freep(&s->intra_pred_data[0]);
     for (i = 0; i < s->s.h.tiling.tile_cols; i++) {
-        av_freep(&s->td[i].c);
+        av_freep(&s->td[i].c_b);
         av_freep(&s->td[i].b_base);
         av_freep(&s->td[i].block_base);
         s->c_b_size[i] = 0;
