10fdbe8908d47cc3e00bf486531f12d5d057b299
Ilia Valiakhmetov <zakne0ne@gmail.com>
2017-08-17 04:27:16 +0700

    loopfilter test#28

--- libavcodec/vp9.c
+++ libavcodec/vp9.c
@@ -1245,8 +1245,7 @@ static int loopfilter_proc(AVCodecContext *avctx) {
     //loopfilter one row
     for (i = 0; i < s->sb_rows; i++) {
         pthread_mutex_lock(&s->mutex);
-        av_log(avctx, AV_LOG_DEBUG, "s->s.h.tiling.log2_tile_cols = %d\n", s->s.h.tiling.log2_tile_cols);
-        while (atomic_load_explicit(&s->m_row[i], memory_order_relaxed) != s->s.h.tiling.log2_tile_cols)
+        while (atomic_load_explicit(&s->m_row[i], memory_order_relaxed) != s->s.h.tiling.log2_tile_cols+1)
             pthread_cond_wait(&s->cond, &s->mutex);
         
         if (s->s.h.filter.level) {
