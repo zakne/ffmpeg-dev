c766fb097c82604c989b08d0eca193b5a527ed87
Ilia Valiakhmetov <zakne0ne@gmail.com>
2017-08-17 00:35:35 +0700

    loopfilter test#22 fix#7

--- libavcodec/vp9.c
+++ libavcodec/vp9.c
@@ -1214,10 +1214,10 @@ int decode_tiles(AVCodecContext *avctx, void *tdata, int jobnr,
 
                 atomic_fetch_add_explicit(&s->m_row[row/8], 1, memory_order_relaxed);
                 pthread_cond_signal(&s->cond);
+                lflvl_ptr = td->lflvl_ptr+s->sb_cols*((row+8)/8);
                 if (row != 0 && row%10 == 0)
                     lflvl_ptr = td->lflvl_ptr;
-                else
-                    lflvl_ptr = td->lflvl_ptr+s->sb_cols*(row/8);
+              
                 // FIXME maybe we can make this more finegrained by running the
                 // loopfilter per-block instead of after each sbrow
                 // In fact that would also make intra pred left preparation easier?
