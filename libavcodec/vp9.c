557c0a72df0d204b79f992fe58c12945d3e81661
Ilia Valiakhmetov <zakne0ne@gmail.com>
2017-08-18 06:01:14 +0700

    allocate more space for lflvl

--- libavcodec/vp9.c
+++ libavcodec/vp9.c
@@ -176,7 +176,7 @@ static int update_size(AVCodecContext *avctx, int w, int h)
     // FIXME we slightly over-allocate here for subsampled chroma, but a little
     // bit of padding shouldn't affect performance...
     p = av_malloc(s->sb_cols * (128 + 192 * bytesperpixel +
-                                4*sizeof(*s->lflvl) + 16 * sizeof(*s->above_mv_ctx)));
+                                8*sizeof(*s->lflvl) + 16 * sizeof(*s->above_mv_ctx)));
     if (!p)
         return AVERROR(ENOMEM);
     assign(s->intra_pred_data[0],  uint8_t *,             64 * bytesperpixel);
@@ -195,7 +195,7 @@ static int update_size(AVCodecContext *avctx, int w, int h)
     assign(s->above_comp_ctx,      uint8_t *,              8);
     assign(s->above_ref_ctx,       uint8_t *,              8);
     assign(s->above_filter_ctx,    uint8_t *,              8);
-    assign(s->lflvl,               VP9Filter *,            4);
+    assign(s->lflvl,               VP9Filter *,            8);
 #undef assign
 
     // these will be re-allocated a little later
@@ -1211,7 +1211,7 @@ int decode_tiles(AVCodecContext *avctx, void *tdata, int jobnr,
                 }
                 atomic_fetch_add_explicit(&s->m_row[row/8], 1, memory_order_relaxed);
                 pthread_cond_signal(&s->cond);
-                if (row != 0 && c == 4) {
+                if (row != 0 && c == 8) {
                     lflvl_ptr = td->lflvl_ptr;
                     c = 0;
                 }
@@ -1247,7 +1247,7 @@ static int loopfilter_proc(AVCodecContext *avctx) {
         if (s->s.h.filter.level) {
             yoff2 = (ls_y * 64)*i;
             uvoff2 =  (ls_uv * 64 >> s->ss_v)*i;
-            lflvl_ptr = s->lflvl+s->sb_cols*(i%4);
+            lflvl_ptr = s->lflvl+s->sb_cols*(i%8);
             for (col = 0; col < s->cols;
                  col += 8, yoff2 += 64 * bytesperpixel,
                  uvoff2 += 64 * bytesperpixel >> s->ss_h, lflvl_ptr++) {
