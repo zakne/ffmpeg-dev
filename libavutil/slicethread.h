e696cc579346c8734c7d9afe4b8190fe2720b6b6
Ilia Valiakhmetov <zakne0ne@gmail.com>
2017-09-07 22:30:40 +0700

    fix@8

--- libavutil/slicethread.h
+++ libavutil/slicethread.h
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "avcodec.h"
+#include "libavcodec/avcodec.h"
 
 #ifndef AVUTIL_SLICETHREAD_H
 #define AVUTIL_SLICETHREAD_H
